730331ddde06d53b71827f30abec50ad46db2e1f
mateuszfranke <mateuszfranke@icloud.com>
2020-10-12 17:45:35 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StayComponent } from './stay-list/stay/stay.component';
+import { StayListComponent } from './stay-list/stay-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StayComponent,
+    StayListComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
